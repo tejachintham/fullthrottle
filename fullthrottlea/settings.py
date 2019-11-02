9ce61bd1d35fec79fcce321246e68bcdb376e1de
ram teja <tejachintham@gmail.com>
2019-11-02 11:53:34 +0530

    first

--- fullthrottlea/settings.py
+++ fullthrottlea/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'fullthrottlea.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(os.path.dirname(__file__) ,'../templates').replace('\\','/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
